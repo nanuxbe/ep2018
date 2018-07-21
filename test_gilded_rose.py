cc4fad0481ed3db93b5165a6e64c8635e97fc2f0
Emmanuelle Delescolle <emma@levit.be>
2018-07-21 18:14:34 +0200

    :heavy_plus_sign: test helper functions

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,14 +1,53 @@
 # -*- coding: utf-8 -*-
+from copy import deepcopy
+
 import unittest
 
 from gilded_rose import Item, GildedRose
+from data import items
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+
+    @property
+    def regular_items(self):
+        return [item
+                for item in items
+                if item.name.split(' ')[0].lower() not in (
+                    'aged', 'conjured', 'backstage', 'sulfuras,'
+                )]
+
+    @property
+    def aged_items(self):
+        return [item
+                for item in items
+                if item.name.split(' ')[0].lower() == 'aged']
+
+    @property
+    def sulfuras_items(self):
+        return [item
+                for item in items
+                if item.name.split(' ')[0].lower() == 'sulfuras,']
+
+    @property
+    def conjured_items(self):
+        return [item
+                for item in items
+                if item.name.split(' ')[0].lower() == 'conjured']
+
+
+    def get_attr_diff(self, items, attr='quality', count=1):
+        original = [getattr(item, attr)
+                    for item in items]
+
+        gr = GildedRose(items)
+        for i in range(count):
+            gr.update_quality()
+
+        result = [getattr(item, attr)
+                  for item in items]
+
+        return zip(original, result)
+
 
 if __name__ == '__main__':
     unittest.main()
